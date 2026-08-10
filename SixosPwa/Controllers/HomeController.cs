cfdeab779c6f56c9f8819c3ed20afa9e1ce5f14f
ThienHiTech <thienhitech@gmail.com>
2026-08-10 16:52:17 +0700

    .

--- SixosPwa/Controllers/HomeController.cs
+++ SixosPwa/Controllers/HomeController.cs
@@ -21,6 +21,12 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpGet]
+    public IActionResult GuiTinNhan()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
